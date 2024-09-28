3b432256c9b3c33ec081eaa6951d1e56ec82ccaf
NamanGupta123 <namangupta0330@gmail.com>
2024-09-28 23:07:36 +0530

    Fixed API issue

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 os.environ["REPLICATE_API_TOKEN"] = st.secrets["api"]["REPLICATE_API_TOKEN"]
-replicate_id = st.secrets["id"]["REPLICATE_ID"]
+# replicate_id = st.secrets["id"]["REPLICATE_ID"]
 
 llama2_chat_replicate = Replicate(
-    model=replicate_id, model_kwargs={"temperature": 0.06, "max_length": 700, "top_p": 1}
+    model="meta/llama-2-13b-chat", model_kwargs={"temperature": 0.02, "max_length": 700, "top_p": 1}
 )
 
 llm = llama2_chat_replicate
