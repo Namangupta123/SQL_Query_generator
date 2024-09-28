501c1f02efaa52e64d01e1b2dbb71368786a7c8c
NamanGupta123 <namangupta0330@gmail.com>
2024-09-28 17:27:28 +0530

    Added Support for Streamlit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain_community.llms import Replicate
 import os
 from langchain_core.prompts import ChatPromptTemplate
@@ -5,11 +6,11 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 load_dotenv()
-os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_API_TOKEN")
+os.environ["REPLICATE_API_TOKEN"] = "r8_4btmPXStMhf2rBdK87sMqUlcl8BdEMD1BzlDO"
 
 replicate_id = "meta/llama-2-13b-chat:f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d"
 llama2_chat_replicate = Replicate(
-    model=replicate_id, model_kwargs={"temperature": 0.01, "max_length": 500, "top_p": 1}
+    model=replicate_id, model_kwargs={"temperature": 0.06, "max_length": 700, "top_p": 1}
 )
 
 llm = llama2_chat_replicate
@@ -27,31 +28,48 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-
-def get_schema():
-    print("Enter the schema of the database (type 'done' when finished):")
-    schema_lines = []
-    while True:
-        line = input()
-        if line.lower() == "done":
-            break
-        schema_lines.append(line)
-    return "\n".join(schema_lines)
-
-
-def get_question():
-    question = input("Enter the question: ")
-    return question
-
-schema = get_schema()
-question = get_question()
-
-input_data = {"schema": schema, "question": question}
-sql_response = (
-    prompt
-    | llm.bind(stop=["\nSQLResult:"])
-    | StrOutputParser()
+st.set_page_config(
+    page_title="SQL Gen",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        'About': 'Hello'
+    }
 )
-result = sql_response.invoke(input_data)
-print("Generated SQL Query:")
-print(result)
+
+def main():
+    st.title("SQL Gen")
+    st.write("A simple tool to generate SQL queries")
+    schema = st.text_area("Enter schema", key="schema")
+    if schema:
+        generate(schema)
+    
+
+def generate(schema):
+    question = st.text_input("Enter your query", key="query")
+
+    if st.button("Generate SQL"):
+        if not question:
+            st.error("Please enter question !!")
+        else:
+            with st.spinner("Please wait for a few seconds :)"):
+                try:
+                    input_data = {"schema": schema, "question": question}
+                    sql_response = (
+                        prompt
+                        | llm.bind(stop=["\nSQLResult:"])
+                        | StrOutputParser()
+                    )
+                    result = sql_response.invoke(input_data)
+                    
+                    st.success("SQL query generated successfully.")
+                    st.code(result, language="sql")
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
+
+    if st.button("RESET"):
+        st.rerun()
+
+if __name__ == "__main__":
+    main()
